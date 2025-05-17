d3cb065dfbc414b4f59f4fe9fc0ac4ede9df0974
Daniel-iel <iel_182@hotmail.com>
2025-05-17 18:15:58 -0300

    add explicação

--- maxSubarraySum/main_five.js
+++ maxSubarraySum/main_five.js
@@ -16,7 +16,24 @@ function recursion(arr, num, currentIndex, temp, counter, total) {
     counter++;
 
     if (counter === num) {
-        total = Math.max(total, temp);    
+        total = Math.max(total, temp);
+        /*
+            Técnica: Sliding Window
+
+            O que está acontecendo aqui?
+            Essa linha está deslizando a janela de soma para frente, removendo o primeiro elemento da janela atual.
+            Exemplo para ilustrar:
+            Suponha num = 3 e arr = [1, 2, 3, 4].
+
+            Quando currentIndex = 3, a janela atual cobre os elementos:
+            [2, 3, 4]
+            A soma temp = 2 + 3 + 4 = 9
+
+            Mas agora queremos mover a janela 1 elemento pra frente, ou seja, passar de [2, 3, 4] para [3, 4, ?]
+            Para isso, removemos o primeiro valor da janela atual, que é arr[3 - 3 + 1] = arr[1] = 2
+            Então fazemos temp -= arr[1] → temp = 9 - 2 = 7
+            A próxima iteração adicionará o novo elemento à direita da janela
+        */
         temp -= arr[currentIndex - num + 1];
         counter--;
     }
